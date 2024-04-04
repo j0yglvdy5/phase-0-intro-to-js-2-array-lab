92e55124a9d0da68cc4c0383014a44045a40b0aa
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-04-04 12:40:56 +0300

    phase 0

--- index.js
+++ index.js
@@ -1,1 +1,90 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat
+};
+
+
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+module.exports = {
+  cats,
+  destructivelyPrependCat
+};
+
+
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+module.exports = {
+  cats,
+  destructivelyRemoveLastCat
+};
+
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+module.exports = {
+  cats,
+  destructivelyRemoveFirstCat
+};
+
+function appendCat(name) {
+  let newCats = [...cats, name];
+  return newCats;
+}
+
+module.exports = {
+  cats,
+  appendCat
+};
+
+
+function prependCat(name) {
+  let newCats = [name, ...cats];
+  return newCats;
+}
+
+module.exports = {
+  cats,
+  prependCat
+};
+
+
+function removeLastCat() {
+  let newCats = cats.slice(0, -1);
+  return newCats;
+}
+
+module.exports = {
+  cats,
+  removeLastCat
+};
+
+function removeFirstCat() {
+  let newCats = cats.slice(1);
+  return newCats;
+}
+
+module.exports = {
+  cats,
+  removeFirstCat
+};
+
+
+
+
+
+
